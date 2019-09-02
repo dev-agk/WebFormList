7d0bfe6911929940870d26cf76b01a5206fc72d3
dev-agk <53688234+dev-agk@users.noreply.github.com>
2019-09-02 19:30:18 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebItemOfListDemo.Models;
+
+namespace WebItemOfListDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new List<WebItemOfListDemo.Models.Item>();
+            model.Add(new Item()
+            {
+                Guid = Guid.NewGuid().ToString(),
+                a = "a1",
+                b = "b1",
+                c = "c1",
+                d = "d1",
+                istrue = true
+            });
+            model.Add(new Item()
+            {
+                Guid = Guid.NewGuid().ToString(),
+                a = "a2",
+                b = "b2",
+                c = "c2",
+                d = "d2",
+                istrue = false
+            });
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult someaction(Item s)
+        {
+            return Json(s);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebItemOfListDemo.Models
+{
+    public class Item
+    {
+        public Item()
+        {
+
+        }
+        public string Guid { get; set; }
+        public string a { get; set; }
+        public string b { get; set; }
+        public string c { get; set; }
+        public string d { get; set; }
+        public bool istrue { get; set; }
+    }
+}
